20b37fbc8854ba711a88d29d5314c5e70d0e52fe
xxy <xuxiaoyuspallow@gmail.com>
2018-05-06 18:43:50 +0800

    test createblockchain

--- main.go
+++ main.go
@@ -1,17 +1,48 @@
 package main
 
+import (
+	"github.com/boltdb/bolt"
+	"log"
+	"fmt"
+	"bytes"
+)
+
+func test()  {
+	db, err := bolt.Open(dbFile,0600,nil)
+	if err!=nil{
+		log.Panic(err)
+	}
+	//err = db.Update(func(tx *bolt.Tx) error {
+	//	//cbtx := NewCoinbaseTX("Ivan", genesisCoinbaseData) //创世coinbase
+	//	//genesisBlock := NewGenesisBlock(cbtx)              //创世块
+	//
+	//	bucket, err := tx.CreateBucket([]byte(blocksBucket))
+	//	if err != nil {
+	//		log.Panic(err)
+	//	}
+	//	//vv2 := genesisBlock.Transactions[0]
+	//	//fmt.Println(vv2)
+	//	err = bucket.Put([]byte("name"), []byte("value"))
+	//	if err != nil {
+	//		log.Panic(err)
+	//	}
+	//	return nil
+	//})
+	db.Close()
+	db, err = bolt.Open(dbFile,0600,nil)
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		tip := b.Get([]byte("name"))
+		//value := b.Get(tip)
+		//valu1 := DeserializeBlock(tip)
+		n := bytes.IndexByte(tip, 0)
+		//vv2 := valu1.Transactions[0]
+		fmt.Println(n)
+		return nil
+	})
+}
 
 func main() {
 	cli := CLI{}
 	cli.Run()
-	//db, err := bolt.Open(dbFile,0600,nil)
-	//if err != nil {
-	//	log.Panic(err)
-	//}
-	//err = db.View(func(tx *bolt.Tx) error {
-	//	b := tx.Bucket([]byte(blocksBucket))
-	//	tip := b.Get([]byte("1"))
-	//	fmt.Println(tip)
-	//	return nil
-	//})
 }
